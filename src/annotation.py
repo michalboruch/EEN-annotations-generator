7b126abc9e0d235486731e96f99820706296d71f
MB <michal@boruch.xyz>
2021-02-12 14:22:12 +0100

    Add timeline display property.

--- src/annotation.py
+++ src/annotation.py
@@ -25,6 +25,10 @@ def get_random_label():
     return random.choice(labels)
 
 
+def get_random_display():
+    return random.choice(["tick", "span", "none"])
+
+
 def get_random_thickness():
     return random.choice(["thin", "normal", "thick"])
 
@@ -36,15 +40,17 @@ def get_random_color(chars="ABCDEF" + string.digits):
 def get_random_floats(start=0, stop=1):
     return sorted([random.uniform(start, stop), random.uniform(start, stop)])
 
+
 def get_random_text():
     return random.choice(texts)
+
 
 def get_annotation_data():
     return json.dumps(
         {
             "_s": {
                 "b": get_box_dimensions(),
-                "d": "",
+                "d": get_random_display(),  # Timeline Display
                 "l": get_random_label(),
                 "lt": get_random_thickness(),
                 "sc": get_random_color(),
